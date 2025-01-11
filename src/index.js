c2c43ddec195541f51a1eef9b0a9ca3d5d164da1
Aduggal1163 <abhishekduggal04@gmail.com>
2025-01-11 16:22:10 +0530

    Basic controllers and routes info

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import express from 'express'
-import mongoose from 'mongoose'
 import dotenv from 'dotenv';
 import DBconnect from './db/index.js';
+import {app} from './app.js'
 dotenv.config();
-const app=express();
 DBconnect().then(()=>{
     app.listen(process.env.PORT,()=>{
         console.log("server is running at port:",process.env.PORT);
